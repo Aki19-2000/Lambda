2ff5a044d7084150a0f94838d505042b9cf94e72
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 19:42:46 +0530

    Update variables.tf

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -2,13 +2,25 @@ variable "lambda_function_name" {
   description = "Name of the Lambda function"
   type        = string
 }
- 
-variable "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  type        = string
-}
- 
+
 variable "iam_role_arn" {
   description = "ARN of the IAM role for Lambda"
   type        = string
+}
+
+variable "image_uri" {
+  description = "URI for the container image in ECR"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment variable for Lambda (dev, prod, etc.)"
+  type        = string
+  default     = "dev"
+}
+
+variable "dlq_arn" {
+  description = "ARN of the Dead Letter Queue (optional)"
+  type        = string
+  default     = ""  # Default value is empty, can be updated when required
 }
