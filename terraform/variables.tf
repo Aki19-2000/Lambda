48dd7926d6e7e430047af9c6eada1c8a5575fcf3
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 21:37:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "account_id" {
+  description = "The AWS Account ID"
+  type        = string
+  default     = ""  # You can leave it blank or hardcode your AWS account ID here
+}
