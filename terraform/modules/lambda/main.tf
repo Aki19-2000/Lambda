bfc839ffa69b2558b996559558c9b219680b6877
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 19:42:23 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -2,19 +2,28 @@ resource "aws_lambda_function" "this" {
   function_name = var.lambda_function_name
   role          = var.iam_role_arn
   package_type  = "Image"
-  image_uri     = "510278866235.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest"
- 
+  image_uri     = var.image_uri
+
   environment {
     variables = {
-      ENV = "dev"
+      ENV = var.environment
     }
   }
+
+  # Add Dead Letter Queue and Retry Configuration for better fault tolerance (optional but recommended)
+  dead_letter_config {
+    target_arn = var.dlq_arn
+  }
+
+  retry {
+    attempts = 2  # Number of retry attempts in case of failure
+  }
 }
- 
+
 output "lambda_invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
 }
- 
+
 output "lambda_function_arn" {
   value = aws_lambda_function.this.arn
 }
