c2127b290cd5271b7db8a37445e8c60fa1429b00
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 14:14:43 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,23 +1,3 @@
-variable "patient_service_image_uri" {
-  description = "The ECR repository URL for the patient service Lambda function"
-  type        = string
-}
-
-variable "appointment_service_image_uri" {
-  description = "The ECR repository URL for the appointment service Lambda function"
-  type        = string
-}
-
-variable "private_subnet_ids" {
-  description = "The subnet IDs where Lambda will run"
-  type        = list(string)
-}
-
-variable "lambda_security_group_id" {
-  description = "The security group ID for Lambda functions"
-  type        = string
-}
-
 # Lambda execution IAM role
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda-execution-role"
@@ -31,45 +11,6 @@ resource "aws_iam_role" "lambda_exec_role" {
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-      }
-    ]
-  })
-}
-
-# IAM Policy for Lambda execution (write logs to CloudWatch and access ECR)
-resource "aws_iam_role_policy" "lambda_exec_policy" {
-  name = "lambda-execution-policy"
-  role = aws_iam_role.lambda_exec_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer"
-        ]
-        Effect   = "Allow"
-        Resource = var.patient_service_image_uri
-      },
-      {
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer"
-        ]
-        Effect   = "Allow"
-        Resource = var.appointment_service_image_uri
       }
     ]
   })
@@ -117,4 +58,13 @@ resource "aws_lambda_function" "appointment_service" {
     subnet_ids         = var.private_subnet_ids
     security_group_ids = [var.lambda_security_group_id]
   }
+}
+
+# Output the Lambda invoke ARN for use in root module
+output "patient_service_invoke_arn" {
+  value = aws_lambda_function.patient_service.invoke_arn
+}
+
+output "appointment_service_invoke_arn" {
+  value = aws_lambda_function.appointment_service.invoke_arn
 }
