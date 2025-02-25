4e31d5e47becdd290aa5334faabed557da0e5072
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 16:40:05 +0530

    Update and rename app.js to app.js

--- services/patient-service/app.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  console.log("Patient service triggered", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Patient Service!",
-    }),
-  };
-};
