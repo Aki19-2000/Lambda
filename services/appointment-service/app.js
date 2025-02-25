976cbdcf76d0c4dfb053588e2fa306ad26367cdf
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 16:31:27 +0530

    Delete services/appointment-service/app.js

--- services/appointment-service/app.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  console.log("Appointment service triggered", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Appointment Service!",
-    }),
-  };
-};
