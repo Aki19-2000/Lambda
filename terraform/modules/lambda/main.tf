c4a44a2cca49ab4df168914466099b09bba5d7a7
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 18:41:49 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,60 +1,20 @@
-main.tf
- 
 resource "aws_lambda_function" "this" {
-
   function_name = var.lambda_function_name
-
   role          = var.iam_role_arn
-
   package_type  = "Image"
-
   image_uri     = "${var.ecr_repository_url}:latest"
  
   environment {
-
     variables = {
-
       ENV = "dev"
-
     }
-
   }
-
 }
  
 output "lambda_invoke_arn" {
-
   value = aws_lambda_function.this.invoke_arn
-
 }
  
 output "lambda_function_arn" {
-
   value = aws_lambda_function.this.arn
-
 }
- 
-variable "lambda_function_name" {
-
-  description = "Name of the Lambda function"
-
-  type        = string
-
-}
- 
-variable "ecr_repository_url" {
-
-  description = "URL of the ECR repository"
-
-  type        = string
-
-}
- 
-variable "iam_role_arn" {
-
-  description = "ARN of the IAM role for Lambda"
-
-  type        = string
-
-}
- 
