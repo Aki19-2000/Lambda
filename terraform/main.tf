68990eaf526bd0d93586b7a945fb2d146916ff88
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 16:28:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ module "lambda" {
 
   # Pass the ECR image URIs and VPC subnet IDs/security group IDs
   patient_service_image_uri    = "510278866235.dkr.ecr.us-east-1.amazonaws.com/patient-service:latest"
-  appointment_service_image_uri = "510278866235.dkr.ecr.us-east-1.amazonaws.com/appointment-service:latest"
   
   private_subnet_ids          = module.vpc.private_subnet_ids
   lambda_security_group_id    = module.vpc.lambda_security_group_id
@@ -54,36 +53,3 @@ resource "aws_api_gateway_deployment" "patient_service_deployment" {
   stage_name  = "prod"
 }
 
-# API Gateway for Appointment Service Lambda
-resource "aws_api_gateway_rest_api" "appointment_service_api" {
-  name        = "appointment-service-api"
-  description = "API for Appointment Service Lambda"
-}
-
-resource "aws_api_gateway_resource" "appointment_service_resource" {
-  rest_api_id = aws_api_gateway_rest_api.appointment_service_api.id
-  parent_id   = aws_api_gateway_rest_api.appointment_service_api.root_resource_id
-  path_part   = "appointment-service"
-}
-
-resource "aws_api_gateway_method" "appointment_service_method" {
-  rest_api_id   = aws_api_gateway_rest_api.appointment_service_api.id
-  resource_id   = aws_api_gateway_resource.appointment_service_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "appointment_service_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.appointment_service_api.id
-  resource_id             = aws_api_gateway_resource.appointment_service_resource.id
-  http_method             = aws_api_gateway_method.appointment_service_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = module.lambda.appointment_service_invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "appointment_service_deployment" {
-  depends_on = [aws_api_gateway_integration.appointment_service_integration]
-  rest_api_id = aws_api_gateway_rest_api.appointment_service_api.id
-  stage_name  = "prod"
-}
