7475d078127e84b69ba8977773bb69c294ab7ee1
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 21:13:00 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -11,6 +11,43 @@ resource "aws_lambda_function" "this" {
   }
 }
  
+resource "aws_api_gateway_rest_api" "this" {
+  name        = "${var.lambda_function_name}-api"
+  description = "API Gateway to trigger ${var.lambda_function_name}"
+}
+
+resource "aws_api_gateway_resource" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "invoke"
+}
+
+resource "aws_api_gateway_method" "this" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.this.id
+  http_method             = aws_api_gateway_method.this.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
+}
+
+# Grant API Gateway permissions to invoke the Lambda function
+resource "aws_lambda_permission" "this" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+output "api_gateway_url" {
+  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/prod/invoke"
+}
 output "lambda_invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
 }
