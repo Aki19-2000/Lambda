828e2f52813f9f9d30b04acf715f519ddc5b5e91
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 21:20:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,21 +3,27 @@ provider "aws" {
   region = var.region
 }
 
-# IAM module reference
-module "iam" {
-  source = "./modules/iam"
-  lambda_function_name = "myLambdaFunction"
-}
-
 # Lambda module reference
 module "lambda" {
   source = "./modules/lambda"
   lambda_function_name = "myLambdaFunction"
   iam_role_arn         = module.iam.lambda_role_arn
   image_uri            = "510278866235.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest"
-  environment          = "dev"  # Can be changed to prod, staging, etc.
+  environment          = "dev"
+  dlq_arn              = ""  # Leave empty if you don't want to use a DLQ
+  region               = var.region  # Pass the region here
+}
+
+# IAM module reference
+module "iam" {
+  source = "./modules/iam"
+  lambda_function_name = "myLambdaFunction"
 }
 
 output "lambda_function_arn" {
   value = module.lambda.lambda_function_arn
+}
+
+output "api_gateway_url" {
+  value = module.lambda.api_gateway_url
 }
