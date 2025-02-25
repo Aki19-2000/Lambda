975a90f7065c636bdaea49a9a6540d8b17cde985
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 16:12:51 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -10,40 +10,4 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
-
-# Private Subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-}
-
-# Security Group for Lambda
-resource "aws_security_group" "lambda_sg" {
-  vpc_id = aws_vpc.main.id
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-  }
-}
-
-# Output VPC and Subnet IDs for use in Lambda Module
-output "private_subnet_ids" {
-  value = [aws_subnet.private_subnet.id]
-}
-
-output "lambda_security_group_id" {
-  value = aws_security_group.lambda_sg.id
-}
+ 
