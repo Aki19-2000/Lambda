27abd5d78c2abd0289edde196148251e4bee9f45
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 21:01:42 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -2,27 +2,19 @@ resource "aws_lambda_function" "this" {
   function_name = var.lambda_function_name
   role          = var.iam_role_arn
   package_type  = "Image"
-  image_uri     = var.image_uri
-
+  image_uri     = "510278866235.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest"
+ 
   environment {
     variables = {
-      ENV = var.environment
-    }
-  }
-
-  # Conditionally add Dead Letter Queue configuration if dlq_arn is set
-  dynamic "dead_letter_config" {
-    for_each = var.dlq_arn != "" ? [1] : []
-    content {
-      target_arn = var.dlq_arn
+      ENV = "dev"
     }
   }
 }
-
+ 
 output "lambda_invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
 }
-
+ 
 output "lambda_function_arn" {
   value = aws_lambda_function.this.arn
 }
