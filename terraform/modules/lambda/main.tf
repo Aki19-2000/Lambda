287c3e934321fe137fac23058330ea770321fa53
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 22:23:27 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -78,7 +78,7 @@ resource "aws_api_gateway_stage" "this" {
   }
 
   # Optional: Enable caching if needed
-  caching_enabled = true
+  caching_enabled      = true
   cache_ttl_in_seconds = 3600
 }
 
