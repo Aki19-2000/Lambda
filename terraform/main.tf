d84e9bb188832ea92d65dc9add6d546e7738a9bc
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 19:43:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,22 @@
 # Provider configuration for AWS
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 # Lambda module reference
+module "iam" {
+  source = "./modules/iam"
+  lambda_function_name = "myLambdaFunction"
+}
+
 module "lambda" {
   source = "./modules/lambda"
+  lambda_function_name = "myLambdaFunction"
+  iam_role_arn         = module.iam.lambda_role_arn
+  image_uri            = "510278866235.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest"
+  environment          = "dev"  # Can be changed to prod, staging, etc.
+}
+
+output "lambda_function_arn" {
+  value = module.lambda.lambda_function_arn
 }
