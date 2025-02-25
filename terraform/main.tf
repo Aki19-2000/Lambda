76f6363142d512fab743f513c4ec9bd8e2fb7630
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-25 15:22:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 # Lambda Module
 module "lambda" {
   source = "./modules/lambda"
-  private_subnet_ids         = module.vpc.private_subnet_ids
-  lambda_security_group_id   = module.vpc.lambda_security_group_id
-  patient_service_image_uri  = "510278866235.dkr.ecr.us-east-1.amazonaws.com/patient-service:latest"
+
+  # Pass the correct arguments from the VPC module and ECR image URLs
+  patient_service_image_uri    = "510278866235.dkr.ecr.us-east-1.amazonaws.com/patient-service:latest"
   appointment_service_image_uri = "510278866235.dkr.ecr.us-east-1.amazonaws.com/appointment-service:latest"
 }
 
